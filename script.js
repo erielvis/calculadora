eea0d497a91d3d2b83bbd02653265541ec309dfa
erielvis <erielvis.geffer@escola.pr.gov.br>
2025-08-05 08:59:21 -0300

    .

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let expressao = '';
 
 function adicionar(valor) {
-expressao += valor;
-document.getElementById('visor').innerText = expressao;
+  expressao += valor;
+  document.getElementById('visor').innerText = expressao;
 }
 
 function limpar() {
-expressao = '';
-document.getElementById('visor').innerText = '0';
+  expressao = '';
+  document.getElementById('visor').innerText = '0';
 }
 
 function calcular() {
-try {
-const resultado = eval(expressao);
-document.getElementById('visor').innerText = resultado;
-expressao = resultado.toString();
-} catch (e) {
-document.getElementById('visor').innerText = 'Erro';
-expressao = '';
-}
+  try {
+    const resultado = eval(expressao);
+    document.getElementById('visor').innerText = resultado;
+    expressao = resultado.toString();
+  } catch (e) {
+    document.getElementById('visor').innerText = 'Erro';
+    expressao = '';
+  const resultado = eval(expressao);
+    document.getElementById('visor').innerText = resultado;
+    expressao = resultado.toString();}
 }
